5271d7dd2a04f2a2fa7c1e4fbee2b528073706a4
Zenike <nicolasmaquet@gmail.com>
2018-09-25 19:36:08 +0200

    encapsulate in an autoinvoked function

--- startpack.js
+++ startpack.js
@@ -1,3 +1,4 @@
+(function($){
 
 //////////////////////////////////////////////////////////////////////////////////////
 // Fast scroll to
@@ -197,35 +198,8 @@ function autoloader(cmd){
 //////////////////////////////////////////////////////////////////////////////////////
 // Mobilemenu
 //////////////////////////////////////////////////////////////////////////////////////
-$("body").on("click",".autopopup .close",function() {
-	close_popup($(this).parents(".autopopup"));
-});
-
-// Close by cross
-$("[data-stp-menu-close]").on("click", function() {
-	var menu = $(this).closest("[data-stp-menu]");
-
-	close_stp_menu(menu);
-	return false;
-});
-
-// Close by mask click
-$("body").on("click","#stp-sidemenu-mask",function() {
-	var menu = $(".stp-menu-open");
-
-	close_stp_menu(menu);
-});
-
-// Open by button
-$("[data-stp-menu-open]").click(function() {
-	var id_menu = $(this).attr("data-stp-menu-open");
-
-	show_stp_menu(id_menu);
-	return false;
-});
-
 function show_stp_menu(id){
-
+	// select the target menu
 	var menu = $("[data-stp-menu=" + id + "]");
 
 	if($("#stp-sidemenu-mask").length > 0) {
@@ -241,10 +215,39 @@ function show_stp_menu(id){
 	$("#stp-sidemenu-mask").fadeIn();
 }
 
-function close_stp_menu(menu){
-	// Close the menu by removing a class
-	menu.removeClass("stp-menu-open");
+$(document).ready(function() {
+	function close_stp_menu(menu){
+		// Close the menu by removing a class
+		menu.removeClass("stp-menu-open");
 
-	// Fade OUT stp-sidemenu-mask
-	$("#stp-sidemenu-mask").fadeOut();
-}
+		// Fade OUT stp-sidemenu-mask
+		$("#stp-sidemenu-mask").fadeOut();
+	}
+
+	// Open by button
+	$("[data-stp-menu-open]").click(function() {
+		var id_menu = $(this).attr("data-stp-menu-open");
+
+		show_stp_menu(id_menu);
+		return false;
+	});
+
+	// Close by cross
+	$("[data-stp-menu-close]").on("click", function() {
+		var menu = $(this).closest("[data-stp-menu]");
+
+		close_stp_menu(menu);
+		return false;
+	});
+
+	// Close by mask click
+	$("body").on("click","#stp-sidemenu-mask",function() {
+		var menu = $(".stp-menu-open");
+
+		close_stp_menu(menu);
+	});
+});
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+})(jQuery);
